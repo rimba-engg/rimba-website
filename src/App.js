023b4c37e74c3526300b5aa090cf0c3de501281f
lokesh-danu <lokeshdanu9@gmail.com>
2024-11-18 10:26:02 +0530

    base setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './components/HomePage/HomePage'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomePage></HomePage>
+      </header>
+    </div>
+  );
+}
+
+export default App;
