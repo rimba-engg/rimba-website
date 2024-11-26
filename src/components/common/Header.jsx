f7fc6df7460abbb47d9a585e6e979c48ed3b0cd1
lokesh-danu <lokeshdanu9@gmail.com>
2024-11-26 17:01:58 +0530

    logo redirect to home

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -15,12 +15,14 @@ function Header() {
 
   return (
     <header className="flex flex-wrap gap-5 justify-between self-stretch px-20 py-4 w-full bg-white bg-opacity-40 shadow-[0px_4px_20px_rgba(209,234,232,0.3)] max-md:px-5 max-md:max-w-full">
-      <img
-        loading="lazy"
-        src="https://cdn.builder.io/api/v1/image/assets/TEMP/8e49f884a54337781454ce8d21c6fa0c281e0e6c5e3ca6cc2562ffe4f58afdb9?placeholderIfAbsent=true&apiKey=fd087a8e03ab4599addc57757630651c"
-        className="object-contain shrink-0 self-start max-w-full aspect-[3.04] w-[140px]"
-        alt="Company logo"
-      />
+      <a href="/">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/8e49f884a54337781454ce8d21c6fa0c281e0e6c5e3ca6cc2562ffe4f58afdb9?placeholderIfAbsent=true&apiKey=fd087a8e03ab4599addc57757630651c"
+          className="object-contain shrink-0 self-start max-w-full aspect-[3.04] w-[140px]"
+          alt="Company logo"
+        />
+      </a>
       <nav className="flex flex-wrap gap-8 justify-center items-center my-auto text-base text-slate-950 text-opacity-60 max-md:max-w-full">
         <a
           href="/"
